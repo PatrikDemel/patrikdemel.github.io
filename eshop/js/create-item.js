965482dc6c851455355629ec7b949db09ce92bcf
Patrik Demel <d20133@oaopava.cz>
2023-01-08 00:00:45 +0100

    added personal productID for local storage save

--- eshop/js/create-item.js
+++ eshop/js/create-item.js
@@ -5,11 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
         let productID = button.dataset.id;
         let productPrize = button.dataset.price;
         let productTitle = button.dataset.title;
+        let productGrams = button.dataset.grams;
         let productQuantity = document.querySelector("input").value;
 
+        let select = document.querySelector("select");
+        let value = select.options[select.selectedIndex].value;
+
         let item = {
-            ID: productID, prize: productPrize, title: productTitle, quantity: productQuantity,
+            ID: productID, prize: productPrize, title: productTitle, quantity: productQuantity, grams: productGrams
         }
+        console.log(item)
+
         localStorage.setItem(productID, JSON.stringify(item));
     };
 })
