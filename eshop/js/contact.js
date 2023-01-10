928e99fefc982a12b58bb81c38a9717860ba6193
Patrik Demel <d20133@oaopava.cz>
2023-01-10 10:48:00 +0100

    eshop deleted

--- eshop/js/contact.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let contactForm = document.getElementById("contact-form");
-    let submittedText = document.getElementById("submitted");
-    contactForm.onsubmit = function () {
-        submittedText.style.display = "block";
-    }
-})
