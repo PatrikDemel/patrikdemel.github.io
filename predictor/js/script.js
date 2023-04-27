1be802280970a8b33c0df8579d9fd768de0693c4
PatrikDemel <d20133@oaopava.cz>
2023-04-27 23:25:46 +0200

    edited

--- predictor/js/script.js
+++ predictor/js/script.js
@@ -1,16 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let absence_button = document.getElementById("absence-button");
+document.addEventListener('DOMContentLoaded', () => {
+  let absence_button = document.getElementById('absence-button');
 
   function absence_calc() {
-    let all_hours = Number(document.getElementById("all-hodiny").value);
-    let missed_hours = Number(document.getElementById("missed-hodiny").value);
-    let percent_number = all_hours / missed_hours;
+    let all_hours = document.getElementById('all-hodiny').value;
+    let missed_hours = document.getElementById('missed-hodiny').value;
+    let percent_number = Number(all_hours) / Number(missed_hours);
     let percents = 100 / percent_number;
+    let percent_result = document.getElementById('result-percents');
 
-    let percent_result = document.getElementById("result-percents");
-
-    percent_result.innerHTML(percents);
+    if (all_hours.length == 0 || missed_hours.length == 0) {
+      percent_result.textContent = 'Vyplň obě pole';
+    } else {
+      for (let i = 0; i < 100; i++) {
+        if (i / missed_hours / 100 === 25) {
+        }
+      }
+    }
   }
-
   absence_button.onclick = absence_calc;
 });
